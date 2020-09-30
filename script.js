9972c04c8c993dbeb26a6a3d37c912963f8a3305
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-09-30 06:25:32 -0400

    Have begun dynamically generating my questions. Still a WIP, will work on adding event listeners later, just concerned with getting the content onto the page.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+//Step 1: Test the javascript file to make sure it's linked correctly
 console.log("WE LIVE BABY!");
+//Step 2: Listen for the click to make the opening page content disappear
+
+//Step 3: Simultaneously, dynamically create the question with buttons that have answer options
+var h1El1 = document.createElement("h1");
+
+
+h1El1.textContent="Commonly used data types DO NOT include:";
+document.body.append(h1El1);
