47c755e4a9e0877120c6b1e8bf4543bcc41fa78a
Ashley Ikeh <ashley.ikeh25@gmail.com>
2020-09-30 16:49:18 -0400

    Added a function and an event listener to clear the opening main content when the start quiz button is clicked.

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 //Step 1: Test the javascript file to make sure it's linked correctly
 console.log("WE LIVE BABY!");
-//Step 2: Listen for the click to make the opening page content disappear
-
+//Step 2: Listen for the click to make the opening page content disappear and starts the timer
+//add event listener//
+//queryselector(select main) to set its visibility property to 0
+// document.querySelector("main").removeChild();
 //Step 3: Simultaneously, dynamically create the question with buttons that have answer options
+document.getElementById("start").addEventListener("click", emptyTheMain);
+function emptyTheMain(){
+    document.getElementById("opening-content").remove("open-heading");
+}
+
+
+//  $("start").click(function(){
+//     $("main").empty();
+// })
 
 //Variables to create shortcuts for the question elements
 var h1El1 = document.createElement("h1");
@@ -16,8 +27,8 @@ var btnEl3 = document.createElement("button");
 var dtEl4 = document.createElement("dt");
 var btnEl4 = document.createElement("button");
 
-
 //Question 1, dynamically generated Elements
+
 h1El1.textContent = "Commonly used data types DO NOT include:";
 document.body.append(h1El1);
 btnEl1.textContent = "1.strings";
@@ -37,96 +48,93 @@ dtEl4.append(btnEl4);
 dlEl.append(dtEl4);
 document.body.append(dlEl);
 
+//this empties out all the content:
+// document.getElementById("opening-content").remove();
 
-//Question 2, dynamically generated Elements
-h1El1.textContent = "The condition in an if/else statement is encolosed within ____________.";
-document.body.append(h1El1);
-btnEl1.textContent = "1.quotes";
-dtEl1.append(btnEl1);
-dlEl.append(dtEl1);
-document.body.append(dlEl);
-btnEl2.textContent = "2.curly brackets";
-dtEl2.append(btnEl2);
-dlEl.append(dtEl2);
-document.body.append(dlEl);
-btnEl3.textContent = "3.parentheses";
-dtEl3.append(btnEl3);
-dlEl.append(dtEl3);
-document.body.append(dlEl);
-btnEl4.textContent = "4.square brackets";
-dtEl4.append(btnEl4);
-dlEl.append(dtEl4);
-document.body.append(dlEl);
+// //Question 2, dynamically generated Elements
+// h1El1.textContent = "The condition in an if/else statement is encolosed within ____________.";
+// document.body.append(h1El1);
+// btnEl1.textContent = "1.quotes";
+// dtEl1.append(btnEl1);
+// dlEl.append(dtEl1);
+// document.body.append(dlEl);
+// btnEl2.textContent = "2.curly brackets";
+// dtEl2.append(btnEl2);
+// dlEl.append(dtEl2);
+// document.body.append(dlEl);
+// btnEl3.textContent = "3.parentheses";
+// dtEl3.append(btnEl3);
+// dlEl.append(dtEl3);
+// document.body.append(dlEl);
+// btnEl4.textContent = "4.square brackets";
+// dtEl4.append(btnEl4);
+// dlEl.append(dtEl4);
+// document.body.append(dlEl);
 
-//Question 3, dynamically generated Elements
-h1El1.textContent = "Arrays in JavaScript can be used to store _____________.";
-document.body.append(h1El1);
-btnEl1.textContent = "1.numbers and strings";
-dtEl1.append(btnEl1);
-dlEl.append(dtEl1);
-document.body.append(dlEl);
-btnEl2.textContent = "2.other arrays";
-dtEl2.append(btnEl2);
-dlEl.append(dtEl2);
-document.body.append(dlEl);
-btnEl3.textContent = "3.booleans";
-dtEl3.append(btnEl3);
-dlEl.append(dtEl3);
-document.body.append(dlEl);
-btnEl4.textContent = "4.all of the above";
-dtEl4.append(btnEl4);
-dlEl.append(dtEl4);
-document.body.append(dlEl);
+// //Question 3, dynamically generated Elements
+// h1El1.textContent = "Arrays in JavaScript can be used to store _____________.";
+// document.body.append(h1El1);
+// btnEl1.textContent = "1.numbers and strings";
+// dtEl1.append(btnEl1);
+// dlEl.append(dtEl1);
+// document.body.append(dlEl);
+// btnEl2.textContent = "2.other arrays";
+// dtEl2.append(btnEl2);
+// dlEl.append(dtEl2);
+// document.body.append(dlEl);
+// btnEl3.textContent = "3.booleans";
+// dtEl3.append(btnEl3);
+// dlEl.append(dtEl3);
+// document.body.append(dlEl);
+// btnEl4.textContent = "4.all of the above";
+// dtEl4.append(btnEl4);
+// dlEl.append(dtEl4);
+// document.body.append(dlEl);
 
-//Question 4, dynamically generated Elements
-h1El1.textContent = "String values must be encolosed within ___________ when being assigned to variables.";
-document.body.append(h1El1);
-btnEl1.textContent = "1.commas";
-dtEl1.append(btnEl1);
-dlEl.append(dtEl1);
-document.body.append(dlEl);
-btnEl2.textContent = "2.curly brackets";
-dtEl2.append(btnEl2);
-dlEl.append(dtEl2);
-document.body.append(dlEl);
-btnEl3.textContent = "3.quotes";
-dtEl3.append(btnEl3);
-dlEl.append(dtEl3);
-document.body.append(dlEl);
-btnEl4.textContent = "4.parentheses";
-dtEl4.append(btnEl4);
-dlEl.append(dtEl4);
-document.body.append(dlEl);
+// //Question 4, dynamically generated Elements
+// h1El1.textContent = "String values must be encolosed within ___________ when being assigned to variables.";
+// document.body.append(h1El1);
+// btnEl1.textContent = "1.commas";
+// dtEl1.append(btnEl1);
+// dlEl.append(dtEl1);
+// document.body.append(dlEl);
+// btnEl2.textContent = "2.curly brackets";
+// dtEl2.append(btnEl2);
+// dlEl.append(dtEl2);
+// document.body.append(dlEl);
+// btnEl3.textContent = "3.quotes";
+// dtEl3.append(btnEl3);
+// dlEl.append(dtEl3);
+// document.body.append(dlEl);
+// btnEl4.textContent = "4.parentheses";
+// dtEl4.append(btnEl4);
+// dlEl.append(dtEl4);
+// document.body.append(dlEl);
 
-//Question 5, dynamically generated Elements
-h1El1.textContent = "A very useful tool used during development and debugging for printing content to the debugger is:";
-document.body.append(h1El1);
-btnEl1.textContent = "1.JavaScript";
-dtEl1.append(btnEl1);
-dlEl.append(dtEl1);
-document.body.append(dlEl);
-btnEl2.textContent = "2.terminal/bash";
-dtEl2.append(btnEl2);
-dlEl.append(dtEl2);
-document.body.append(dlEl);
-btnEl3.textContent = "3.for loops";
-dtEl3.append(btnEl3);
-dlEl.append(dtEl3);
-document.body.append(dlEl);
-btnEl4.textContent = "4.console.log";
-dtEl4.append(btnEl4);
-dlEl.append(dtEl4);
-document.body.append(dlEl);
-
-
+// //Question 5, dynamically generated Elements
+// h1El1.textContent = "A very useful tool used during development and debugging for printing content to the debugger is:";
+// document.body.append(h1El1);
+// btnEl1.textContent = "1.JavaScript";
+// dtEl1.append(btnEl1);
+// dlEl.append(dtEl1);
+// document.body.append(dlEl);
+// btnEl2.textContent = "2.terminal/bash";
+// dtEl2.append(btnEl2);
+// dlEl.append(dtEl2);
+// document.body.append(dlEl);
+// btnEl3.textContent = "3.for loops";
+// dtEl3.append(btnEl3);
+// dlEl.append(dtEl3);
+// document.body.append(dlEl);
+// btnEl4.textContent = "4.console.log";
+// dtEl4.append(btnEl4);
+// dlEl.append(dtEl4);
+// document.body.append(dlEl);
 
 
 
-//Create a for loop that takes the bullet points off the list items (https://www.w3schools.com/html/tryit.asp?filename=tryhtml_lists_unordered_none)
-// var btnTags = document.querySelectorAll("button")
-// for(i=0; i<4; i++){
-// btnTags[i].setAttribute("")    
-// }
+
+
 
 
 
