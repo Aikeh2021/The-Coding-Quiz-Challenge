5b274c0ef219ceb5dc0141bf1c6d6c36c13f8353
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-23 20:28:55 -0500

    Got the overall timer working for the quiz. Set up a mini timer to show the user if they got a question right or wrong. Set up conditionals on the answer choices to trigger different events but end up in calling the function that starts question 2, which for right now only clears the text content in the openingDiv. Also set the overall timer so that at the end of the 60 seconds, it redirects the user to the highscores page and clears the timerInterval until the user starts the quiz again.

--- script.js
+++ script.js
@@ -1,52 +1,139 @@
+//Testing to make sure we're connected
 console.log("le sigh");
 //DOM Variables
 var startQuiz = document.getElementById("start");
 var openingDiv = document.getElementById("opening");
 var contentDiv = document.getElementById("content-div");
 
-
-//JS Variables
+//Object with an array of quiz questions and arrays for each of the answer options for each of the questions
 var quizContent = {
-    quizQuestions: ["Commonly used data types DO NOT include:", "The condition in an if/else statement is enclosed within _________________.", "Arrays in JavaScript can be used to store _______________.", "String values must be enclosed within _______________ when being assigned to variables.", "A very useful tool used during development and debugging for printing content to the debugger is:"],
-    questionOneAnswerOptions: ["1.strings", "2.booleans", "3.alerts", "4.numbers"],
-    questionTwoAnswerOptions: ["1.quotes", "2.curly brackets", "3.parentheses", "4.square brackets"],
-    questionThreeAnswerOptions: ["1.numbers and strings", "2.other arrays", "3.booleans", "4.all of the above"],
-    questionFourAnswerOptions: ["1.commas", "2. curly brackets", "quotes", "4. parentheses"],
-    questionFiveAnswerOptions: ["1.JavaScript", "2.terminal/bash", "3.for loops", "4.console.log"],
+  quizQuestions: [
+    "Commonly used data types DO NOT include:",
+    "The condition in an if/else statement is enclosed within _________________.",
+    "Arrays in JavaScript can be used to store _______________.",
+    "String values must be enclosed within _______________ when being assigned to variables.",
+    "A very useful tool used during development and debugging for printing content to the debugger is:",
+  ],
+  questionOneAnswerOptions: [
+    "1.strings",
+    "2.booleans",
+    "3.alerts",
+    "4.numbers",
+  ],
+  questionTwoAnswerOptions: [
+    "1.quotes",
+    "2.curly brackets",
+    "3.parentheses",
+    "4.square brackets",
+  ],
+  questionThreeAnswerOptions: [
+    "1.numbers and strings",
+    "2.other arrays",
+    "3.booleans",
+    "4.all of the above",
+  ],
+  questionFourAnswerOptions: [
+    "1.commas",
+    "2. curly brackets",
+    "quotes",
+    "4. parentheses",
+  ],
+  questionFiveAnswerOptions: [
+    "1.JavaScript",
+    "2.terminal/bash",
+    "3.for loops",
+    "4.console.log",
+  ],
+};
+
+//Global index variable
+var index = 0;
+
+//Global time variable
+var secondsLeft = 60;
+
+//Function to set and start the timer. Also alerts when the timer is finished
+function setTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    var timer = document.getElementById("time-display");
+    timer.textContent = "Timer: " + secondsLeft + " seconds left";
+    if (secondsLeft <= 0) {
+      clearInterval(timerInterval);
+      alert("GAME OVER!");
+      location.replace("highscore.html");
+    }
+  }, 1000);
 }
 
-
 //Function Definitions
-startQuiz.addEventListener("click", beginTheQuiz);
-function beginTheQuiz(){
-    openingDiv.innerHTML = ("");
-    askQuestionOne();
-};
+startQuiz.addEventListener("click", function () {
+  openingDiv.innerHTML = "";
+  setTime();
+  askQuestionOne();
+});
 
-function askQuestionOne(){
-    var questionsHeadingEl = document.createElement("h1");
-    questionsHeadingEl.setAttribute("id", "questionsHeading");
-    questionsHeadingEl.textContent = quizContent.quizQuestions[0];
-    contentDiv.appendChild(questionsHeadingEl);
-    for(i = 0; i<quizContent.questionOneAnswerOptions.length; i++){
-        console.log(quizContent.questionOneAnswerOptions[0]);
-        var parEl = document.createElement("p");
-        parEl.setAttribute("class", "questions-par-el");
-        var answerButton = document.createElement("button");
-        // quizContent.questionOneAnswerOptions.forEach(addingBtnIds); 
-        // function addingBtnIds() {
-        //     var btnIds = 0;
-        //     btnIds++
-        //     answerButton.setAttribute("id", "" + btnIds)};
-        answerButton.textContent = quizContent.questionOneAnswerOptions[i];
-        parEl.appendChild(answerButton);
-        contentDiv.appendChild(parEl);
-    }
+//Function to put question 1 on screen
+function askQuestionOne() {
+  //Creating the heading that will display a question
+  var questionsHeadingEl = document.createElement("h1");
+  questionsHeadingEl.setAttribute("id", "questionsHeading");
+  questionsHeadingEl.textContent = quizContent.quizQuestions[0];
+  contentDiv.appendChild(questionsHeadingEl);
+  for (i = 0; i < quizContent.questionOneAnswerOptions.length; i++) {
+    var parEl = document.createElement("p");
+    parEl.setAttribute("class", "questions-par-el");
+    var answerButton = document.createElement("button");
+    answerButton.textContent = quizContent.questionOneAnswerOptions[i];
+    index++;
+    answerButton.setAttribute("id", index);
+    parEl.appendChild(answerButton);
+    contentDiv.appendChild(parEl);
+    //Adding event listener to answerbuttons and doing something after one is clicked (starting another timer and letting the user
+    // know their selection was either right or wrong and either increasing the quiz time or decreasing it)
+    answerButton.addEventListener("click", function (event) {
+      if (event.target === document.getElementById("3")) {
+        var miniTimer = 2;
+        function setMiniTime() {
+          var timeInterval = setInterval(function () {
+            miniTimer--;
+            console.log("This is the right answer");
+            var line = document.createElement("hr");
+            var message = document.createElement("p");
+            message.textContent = "You chose the right answer!";
+            contentDiv.append(line, message);
+            if (miniTimer === 0) {
+                secondsLeft = secondsLeft + 4;
+                askQuestionTwo();
+              clearInterval(timeInterval);
+            }
+          }, 1000);
+        }
+        setMiniTime();
+      } else {
+        var miniTimer = 2;
+        function setMiniTime() {
+          var timeInterval = setInterval(function () {
+            miniTimer--;
+            console.log("You chose wrong");
+            var line = document.createElement("hr");
+            var message = document.createElement("p");
+            message.textContent = "Sorry! Better luck next time . . . ";
+            contentDiv.append(line, message);
+            if (miniTimer === 0) {
+                secondsLeft = secondsLeft - 6;
+                askQuestionTwo();
+              clearInterval(timeInterval);
+            }
+          }, 1000);
+        }
+        setMiniTime();
+      }
+    });
+  }
+}
 
-};
-        
-
-
-
-
-//Function Calls
+//Function to put question 2 on screen
+function askQuestionTwo() {
+  contentDiv.textContent = "";
+}
